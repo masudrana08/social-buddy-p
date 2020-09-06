c13f8540eea96540009793f6b74d04b854c142a4
masudrana08 <rana028511@gmail.com>
2020-09-06 06:39:15 +0600

    project is ready

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import './App.css'
+import {BrowserRouter as Router, Route,Switch, Link} from 'react-router-dom'
+import Details from "./Components/Details/Details";
+import Home from "./Components/Home/Home";
+import Appbar from "./Components/Appbar/Appbar";
 function App() {
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <Router>
+      <Appbar></Appbar>
+      
+      <Switch>
+      <Route exact path="/post">
+        <Home></Home>
+      </Route>
+
+      <Route exact path="/">
+        <Home></Home>
+      </Route>
+
+      <Route exact path="/post/:id">
+        <Details></Details>
+      </Route>
+
+    </Switch>
+    
+    
+    </Router>
+
   );
 }
 
 export default App;
+
+//Project Owner : https://github.com/masudrana08
